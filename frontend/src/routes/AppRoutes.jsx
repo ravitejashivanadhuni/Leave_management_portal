ebdcc60a3f48d868bdccb7ebae4af91df2bc4c52
ravitejashivanadhuni <ravitejashivanadhuni@gmail.com>
2026-07-03 21:14:48 +0530

    updated frontend

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LandingPage from "../pages/Landing/LandingPage";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
+
+function AppRoutes() {
+
+    return (
+
+        <BrowserRouter>
+
+            <Routes>
+
+                <Route
+                    path="/"
+                    element={<LandingPage />}
+                />
+
+                <Route
+                    path="/login"
+                    element={<Login />}
+                />
+
+                <Route
+                    path="/register"
+                    element={<Register />}
+                />
+
+            </Routes>
+
+        </BrowserRouter>
+
+    );
+
+}
+
+export default AppRoutes;
